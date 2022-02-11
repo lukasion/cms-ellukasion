f324add9ccaa9cbe20e1b8308cc23e28a46564d8
ellukasion <lukasion@gmail.com>
2022-02-11 18:51:05 +0100

    Make basic BEM structure for SCSS

---
+++ resources/views/content/body.blade.php
@@ -0,0 +1,5 @@
+<main>
+    <div class="body__content container">
+        asd
+    </div>
+</main>
---
+++ resources/views/content/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="header__menu">
+        <div class="container">
+            <div class="header__logo">
+                <img src="https://cdn.freebiesupply.com/logos/large/2x/cms-logo-png-transparent.png" />
+            </div>
+        </div>
+    </div>
+    <div class="header__banner">
+        <img src="" />
+    </div>
+</header>
